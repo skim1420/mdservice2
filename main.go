de3025a4a259a6d4f103593c72fa2d24a052961a
Steven Kim <stevenkim@google.com>
2018-03-21 18:49:34 +0000

    init:

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "io"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", hello)
+  http.ListenAndServe(":80", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  io.WriteString(w, "<body style='background-color: purple'><h1>Hello V2 provider!</h1></body>")
+}
+
