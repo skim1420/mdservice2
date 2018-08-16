557687eda682044996231462b98a01cdb4c1b224
Steven Kim <stevenkim@google.com>
2018-08-16 18:39:26 +0000

    lavender

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "<body style='background-color: skyblue'><h1>Hello V2 provider!</h1></body>")
+  io.WriteString(w, "<body style='background-color: lavender'><h1>Hello V2 provider!</h1></body>")
 }
 
